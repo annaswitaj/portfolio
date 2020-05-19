f35832e4b71f1ceb5435376093d93b125303898d
annaswitaj <annaswitaj.sw@gmail.com>
2020-05-19 21:48:02 +0200

    Updates

---
+++ precache-manifest.7f03c675a5ea5957fd92d97db8483dc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5aa9a443ac4845661efe513e179b6b",
+    "url": "/moja-aplikacja/index.html"
+  },
+  {
+    "revision": "2981695ed14826acf76d",
+    "url": "/moja-aplikacja/static/css/main.2807e4cc.chunk.css"
+  },
+  {
+    "revision": "36b793e30d91142be0ec",
+    "url": "/moja-aplikacja/static/js/2.d628f461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moja-aplikacja/static/js/2.d628f461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2981695ed14826acf76d",
+    "url": "/moja-aplikacja/static/js/main.c9694cf7.chunk.js"
+  },
+  {
+    "revision": "3f2c4f3cf79fd97801b1",
+    "url": "/moja-aplikacja/static/js/runtime-main.93d51f04.js"
+  },
+  {
+    "revision": "4e8f31a4b2e4b433f299c747374142a0",
+    "url": "/moja-aplikacja/static/media/KittyKomunitty.4e8f31a4.png"
+  }
+]);
