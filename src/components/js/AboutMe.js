ba9ab1cfc81aed4fc5cd109114b3e87cffc7e5f3
annaswitaj <annaswitaj.sw@gmail.com>
2020-05-20 01:44:14 +0200

    update information about me

--- src/components/js/AboutMe.js
+++ src/components/js/AboutMe.js
@@ -10,10 +10,10 @@ const AboutMe = () => {
                 <h2 className="title-h1">Kim jestem?</h2>
             </div>
             <div className="description">
-                <article className="des"><p>Nazywam się <strong>Anna Świtaj</strong>. Obecnie jestem na 3 roku studiów kierunku Informatyka, na Uniwersytecie Warmińsko-Mazurskim.</p>
-                    <p>Lubię uczyć się nowych rzeczy.</p>
+                <article className="des"><p>Nazywam się Anna Świtaj. Obecnie jestem na 3 roku studiów kierunku Informatyka, na Uniwersytecie Warmińsko-Mazurskim.</p>
+                    <p>W trakcie studiowania wykonywałam  projekty zespołowe przy użyciu GITa</p>
                 </article>
-                <article className="des">     <p>Szukam stażu lub pracy związanej z programowaniem front-end. Obecnie kształcę się w React i chciałabym się nim zajmować w przyszłości.</p>
+                <article className="des">     <p>Szukam stażu lub pracy związanej z programowaniem front-endu. Obecnie uczę się technologii React i chciałabym dalej się nim zajmować.</p>
                     <p>Hobbistycznie tworzę modele 3D oraz projektuję grafiki.</p>
                 </article>
 
