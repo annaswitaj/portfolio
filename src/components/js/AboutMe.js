b2d5f758592de08ff588a31405428e142338db16
annaswitaj <annaswitaj.sw@gmail.com>
2021-09-08 21:20:20 +0200

    change description

--- src/components/js/AboutMe.js
+++ src/components/js/AboutMe.js
@@ -11,7 +11,7 @@ const AboutMe = () => {
             </div>
             <div className="description">
                 <article className="des"><p>Nazywam się Anna Świtaj. Jestem absolwentem kierunku Informatyka, na Uniwersytecie Warmińsko-Mazurskim.</p>
-                    <p>W trakcie studiowania wykonywałam  projekty zespołowe przy użyciu GITa</p>
+                    <p>W trakcie studiowania wykonywałam  projekty zespołowe przy użyciu GITa.</p>
                 </article>
                 <article className="des">     <p>Szukam stażu/pracy związanej z programowaniem front-endu. Obecnie uczę się technologii React .</p>
                     <p>Hobbistycznie projektuję grafiki, medytuję i praktykuję jogę.</p>
