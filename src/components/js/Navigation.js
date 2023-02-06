fd95f1e1213ce1d841d24adef4de8951d088da6e
annaswitaj <annaswitaj.sw@gmail.com>
2023-02-06 15:42:32 +0100

    fixes

--- src/components/js/Navigation.js
+++ src/components/js/Navigation.js
@@ -1,44 +1,50 @@
-import React from 'react';
-import { Link } from 'react-scroll';
+import React from 'react'
+import { Link } from 'react-scroll'
 import Pineapple from '../../img/pineO.png'
 
-
-const Navigation = (props) => {
-    return (
-        <nav className="App-nav">
-            <img className="App-nav-img" src={Pineapple}></img>
-            <ul className="App-nav-ul">
-                <Link to="App-aboutme"
-                    spy={true}
-                    smooth={true}
-                    offset={-110}
-                    duration={500}>
-                    <li>O mnie</li>
-                </Link>
-                <Link to="App-projects"
-                    spy={true}
-                    smooth={true}
-                    offset={-110}
-                    duration={500}>
-                    <li >Projekty</li>
-                </Link>
-                <Link to="App-technology"
-                    spy={true}
-                    smooth={true}
-                    offset={-110}
-                    duration={500}>
-                    <li >umiejętności</li>
-                </Link>
-                <Link to="App-contact"
-                    spy={true}
-                    smooth={true}
-                    offset={-110}
-                    duration={500}>
-                    <li>Kontakt</li>
-                </Link>
-            </ul>
-
-        </nav>
-    )
+const Navigation = () => {
+  return (
+    <nav className='App-nav'>
+      <img className='App-nav-img' src={Pineapple} alt={'pineapple'}></img>
+      <ul className='App-nav-ul'>
+        <Link
+          to='App-aboutme'
+          spy={true}
+          smooth={true}
+          offset={-110}
+          duration={500}
+        >
+          <li>O mnie</li>
+        </Link>
+        <Link
+          to='App-projects'
+          spy={true}
+          smooth={true}
+          offset={-110}
+          duration={500}
+        >
+          <li>Projekty</li>
+        </Link>
+        <Link
+          to='App-technology'
+          spy={true}
+          smooth={true}
+          offset={-110}
+          duration={500}
+        >
+          <li>umiejętności</li>
+        </Link>
+        <Link
+          to='App-contact'
+          spy={true}
+          smooth={true}
+          offset={-110}
+          duration={500}
+        >
+          <li>Kontakt</li>
+        </Link>
+      </ul>
+    </nav>
+  )
 }
-export default Navigation;
+export default Navigation
