3a094f3191c616946e63e32c0658a5e500f21afd
annaswitaj <annaswitaj.sw@gmail.com>
2023-02-08 16:03:38 +0100

    fixes icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,35 @@
-
-import React from 'react';
-
-
+import React from 'react'
+import { FaPhoneSquare, FaEnvelope } from 'react-icons/fa'
+import { ImLinkedin, ImGithub } from 'react-icons/im'
 
 const Name = () => {
-    return (
-        <section className="App-contact">
-            <div className="title">
-                <h1 className="title-h1">Kontakt</h1>
-  
-            </div>
-            <div className="icons">
-                <div className="container">
-                    <span className="fas fa-phone-square"></span>
-                    <p className="container-data">506-329-928</p>
-                </div>
-                <div className="container">
-                    <span className="fas fa-envelope"></span>
-                    <p className="container-data">annaswitaj.sw@gmail.com</p>
-                </div>
-                <div className="container-icon">
-                    <a href="https://www.linkedin.com/in/anna-%C5%9Bwitaj-a402821aa/"><span className="fab fa-linkedin-in"></span></a>
-                    <a href="https://github.com/annaswitaj"><span className="fab fa-github"></span></a>
-
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className='App-contact'>
+      <div className='title'>
+        <h1 className='title-h1'>Kontakt</h1>
+      </div>
+      <div className='icons'>
+        <div className='container'>
+          <FaPhoneSquare className='icon' />
+          <p className='container-data'>506-329-928</p>
+        </div>
+        <div className='container'>
+          <FaEnvelope className='icon' />
+          <p className='container-data'>annaswitaj.sw@gmail.com</p>
+        </div>
+        <div className='container-icon'>
+          <a
+            href='https://www.linkedin.com/in/anna-%C5%9Bwitaj-a402821aa/'
+            className='linkedin-icon'
+          >
+            <ImLinkedin className='icon' />
+          </a>
+          <a href='https://github.com/annaswitaj'>
+            <ImGithub className='icon' />
+          </a>
+        </div>
+      </div>
+    </section>
+  )
 }
-export default Name;
+export default Name
--- src/components/Technology.js
+++ src/components/Technology.js
@@ -1,23 +1,56 @@
-import React from 'react';
-
+import {
+  SiTypescript,
+  SiGit,
+  SiHtml5,
+  SiReact,
+  SiAngular,
+  SiCss3,
+  SiSass,
+  SiJavascript,
+} from 'react-icons/si'
+import React from 'react'
 
 const Technology = () => {
-    return (
-        <section className="App-technology">
-            <div className="title">
-                <h1 className="title-h1">Umiejętności</h1>
-              
-            </div>
-            <div className="technology-icons">
-                <div className="technology-icon"><span class="fab fa-html5"></span><p>HTML5</p></div>
-                <div className="technology-icon"><span class="fab fa-js"></span><p>JS</p></div>
-                <div className="technology-icon"><span class="fab fa-sass"></span><p>SASS</p></div>
-                <div className="technology-icon"><span class="fab fa-css3-alt"></span><p>CSS3</p></div>
-                <div className="technology-icon"><span class="fab fa-angular"></span><p>Angular</p></div>
-                <div className="technology-icon"><span class="fab fa-git-alt"></span><p>Git</p></div>
-
-            </div>
-        </section>
-    )
+  return (
+    <section className='App-technology'>
+      <div className='title'>
+        <h1 className='title-h1'>Umiejętności</h1>
+      </div>
+      <div className='technology-icons'>
+        <div className='technology-icon'>
+          <SiGit className='icon' />
+          <p>Git</p>
+        </div>
+        <div className='technology-icon'>
+          <SiHtml5 className='icon' />
+          <p>HTML5</p>
+        </div>
+        <div className='technology-icon'>
+          <SiCss3 className='icon' />
+          <p>CSS3</p>
+        </div>
+        <div className='technology-icon'>
+          <SiSass className='icon' />
+          <p>SASS</p>
+        </div>
+        <div className='technology-icon'>
+          <SiJavascript className='icon' />
+          <p>JS</p>
+        </div>
+        <div className='technology-icon'>
+          <SiTypescript className='icon' />
+          <p>TS</p>
+        </div>
+        <div className='technology-icon'>
+          <SiAngular className='icon' />
+          <p>Angular</p>
+        </div>
+        <div className='technology-icon'>
+          <SiReact className='icon' />
+          <p>React</p>
+        </div>
+      </div>
+    </section>
+  )
 }
-export default Technology;
+export default Technology
